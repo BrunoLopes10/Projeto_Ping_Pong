8449422d27cca7b22998fd6a19dade0835d80e62
Galvani <145728055+Galvani027@users.noreply.github.com>
2024-03-16 20:29:02 -0300

    Classe Main

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bem vindo a Java!");
+        Monitor monitor = new Monitor();
+        monitor.run();
+    }
+}
